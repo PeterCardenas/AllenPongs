d5a38a4ba6c8319dcff27798886c04933c5a4f0d
Peter Cardenas <petercardenas441@gmail.com>
2018-07-16 19:00:51 -0500

    Added Multiplier and New Max and Min Pt

--- pingpong.js
+++ pingpong.js
@@ -1,11 +1,16 @@
 const INIT_PT_VAL      = 50;
 const EQ_PT_CHANGE     = 10;
-const MAX_PT_CHANGE    = 50;
-const MIN_PT_CHANGE    = 5;
+//const MAX_PT_CHANGE    = 50;
+const MIN_PT_GAIN      = 10;
+const MIN_PT_LOSS      = 0;
+const STREAK_START     = 3;
 const WIN_STREAK_BASE  = 5;
-const WIN_STREAK_START = 3;
-const LOSS_PERCT_LOSS  = 5;
+const LOSS_PERCT_LOSS  = .05;
 const MIN_PT           = 0;
+const WEAK_MULTIPLIER  = .3;
+const MIN_DIFF_FOR_MAX = 50;
+const NUM_BARS_GRAPH   = 8;
+
 class Player { 
 	constructor(firstName, lastName, ID) {
 		this.firstName = firstName;
@@ -17,7 +22,7 @@ class Player {
 		this.winStreak = 0;
 		this.winStreakBonus = 0;
 		this.lossStreak = 0;
-		this.lossPerct = 100;
+		this.lossPerct = 1;
 	}
 
 	get description() {
@@ -30,25 +35,52 @@ class Player {
 
 	finishMatch(opponent, win) {
 		var change = EQ_PT_CHANGE;
+		var pointDiff = Math.abs(this.points - opponent.points)
+		var max_change = pointDiff / 2;
+
+		if (this.points < opponent.points) {
+			this.points += pointDiff * WEAK_MULTIPLIER;
+		}
+
+		if (pointDiff >= MIN_DIFF_FOR_MAX && change > pointDiff / 2) {
+				change = pointDiff / 2;
+		}
 
 		if (win) {
 			this.matchesWon++;
 			this.winStreak++;
 			this.winStreakBonus += WIN_STREAK_BASE;
 
-			if (this.winStreak >= WIN_STREAK_START) {
+			if (change > 0 && change < MIN_PT_GAIN) {
+				change = MIN_PT_GAIN;
+			}
+
+			if (this.winStreak >= STREAK_START) {
 				change += this.winStreakBonus;
 			}
 
 			if (this.lossStreak > 0) {
-				this.lossPerct = 100;
+				this.lossPerct = 1;
 				this.lossStreak = 0;
 			}
 		}
 		else {
 			this.matchesLost++;
-			change = -change;
 			this.lossStreak++;
+
+			if (change < MIN_PT_LOSS) {
+				change = MIN_PT_LOSS;
+			}
+
+			change = -change;
+
+			if (this.lossPerct > 0) {
+				this.lossPerct -= LOSS_PERCT_LOSS;
+			}
+
+			if (this.lossStreak >= STREAK_START) {
+				change = change * this.lossPerct;
+			}
 
 			if (this.winStreak > 0) {
 				this.winStreak = 0;
@@ -56,23 +88,9 @@ class Player {
 			}
 		}
 
-		if (change > MAX_PT_CHANGE) {
-			change = MAX_PT_CHANGE;
-		}
-
-		else if (change < -MAX_PT_CHANGE) {
-			change = -MAX_PT_CHANGE;
-		}
-
-		else if (change > 0 && change < MIN_PT_CHANGE) {
-			change = MIN_PT_CHANGE;
-		}
-
-		else if (change < 0 && change > -MIN_PT_CHANGE) {
-			change = -MIN_PT_CHANGE;
-		}
-
 		this.points += change;
+
+		this.points = parseInt(this.points, 10);
 
 		if (this.points < MIN_PT) {
 			this.points = MIN_PT;
@@ -100,6 +118,11 @@ class Players {
 
 	get count() {
 		return this.list.length;
+	}
+
+	get graphData() {
+		var max = Math.ceil((this.list[0] / NUM_BARS_GRAPH) / 10) * 10;
+		return null;
 	}
 
 	getPlayerByID(ID) {
@@ -160,6 +183,13 @@ class Simulation extends Players {
 	constructor(numPlayers) {
 		super();
 
+		for (var i = 1; i <= numPlayers; i++) {
+			this.add(new SimPlayer(i));
+		}
+	}
+
+	reset(numPlayers) {
+		this.list = [];
 		for (var i = 1; i <= numPlayers; i++) {
 			this.add(new SimPlayer(i));
 		}
